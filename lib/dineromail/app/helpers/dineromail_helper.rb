1d492e945ca7eaad3635414898550b0cc58be174
Nicolas Mosconi <nicolas@soluciones-simplex.com.ar>
2011-05-28 11:17:52 -0300

    * Added transaction_id to the helper

--- lib/dineromail/app/helpers/dineromail_helper.rb
+++ lib/dineromail/app/helpers/dineromail_helper.rb
@@ -22,17 +22,21 @@ module DineromailHelper
     account_number =  options[:account_number] || Dineromail.configuration.account_number
     pay_methods = options[:pay_methods] || Dineromail.configuration.pay_methods
     currency = options[:currency] || Dineromail.configuration.currency
-      
+    transaction_id = options[:transaction_id]
+    item_number = options[:item_number]
+    
     html = content_tag(:input,nil, :type => 'hidden', :name => 'NombreItem', :value =>item_name)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'TipoMoneda', :value =>currency)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'PrecioItem', :value =>amount)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'E_Comercio', :value =>account_number)
-    html += content_tag(:input,nil, :type => 'hidden', :name => 'NroItem', :value => options[:item_number]) if options.include? :item_number
     html += content_tag(:input,nil, :type => 'hidden', :name => 'image_url', :value =>logo_url)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'DireccionExito', :value => return_url)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'DireccionFracaso', :value => error_url)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'Mensaje', :value => message)
     html += content_tag(:input,nil, :type => 'hidden', :name => 'MediosPago', :value => pay_methods)
+    html += content_tag(:input,nil, :type => 'hidden', :name => 'NroItem', :value => item_number) if item_number
+    html += content_tag(:input,nil, :type => 'hidden', :name => 'TRX_ID', :value => transaction_id) if transaction_id
+    
     html
   end  
   
